2d4eb9b0df73b32def9d6dbd079cc7521b88cbd9
minTwin <michaelmin2020@gmail.com>
2023-10-28 09:29:46 -0700

    created UsersController.cs

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}

c4f23575c7bf0ff2255b17b885463c3d8b442d46
minTwin <michaelmin2020@gmail.com>
2023-11-01 22:33:50 -0700

    added Microsoft.AspNetCore.Authentication.JwtBearer nuget package
    added configurations in Program.cs

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -22,6 +24,7 @@ namespace API.Controllers
 
             return users;
         }
+
 
         [HttpGet("{id}")] // /api/users/3
         public async Task<ActionResult<AppUser>> GetUser(int id)

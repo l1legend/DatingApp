9498af0d77d761e0a4e611c78d8d6550e04e1569
minTwin <michaelmin2020@gmail.com>
2023-11-01 22:47:02 -0700

    added AllowAnonymous decorator to GetUser method in UserController.cs

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -17,6 +17,7 @@ namespace API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {

3a30f8e3266ded84e2c8aa2416c59c9d408645a6
minTwin <michaelmin2020@gmail.com>
2023-11-03 02:32:39 -0700

    created angular form component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  model: any = {}
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  login() {
+    console.log(this.model);
+  }
 }

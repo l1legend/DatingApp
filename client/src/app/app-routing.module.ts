01f8d67902480b7e9f5a27eac7743f5f3979632a
minTwin <michaelmin2020@gmail.com>
2023-11-14 06:03:11 -0800

    setting routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MembersListComponent } from './members/members-list/members-list.component';
+import { MembersDetailComponent } from './members/members-detail/members-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'members', component: MembersListComponent},
+  {path: 'member/:id', component: MembersDetailComponent},
+  {path: 'lists', component: ListsComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: '**', component: HomeComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

c0dfd42bcd9b2e83117a13ca3a55dcd969c00111
minTwin <michaelmin2020@gmail.com>
2023-11-14 05:56:59 -0800

    created list, members-detail, members-list, and message component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,13 +9,21 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MembersListComponent } from './members/members-list/members-list.component';
+import { MembersDetailComponent } from './members/members-detail/members-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MembersListComponent,
+    MembersDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/members/members-detail/members-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-members-detail',
+  templateUrl: './members-detail.component.html',
+  styleUrls: ['./members-detail.component.css']
+})
+export class MembersDetailComponent {
+
+}

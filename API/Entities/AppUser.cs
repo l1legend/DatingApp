b97d06a9b4673d8da3e7676af38ade3ba8594b50
minTwin <michaelmin2020@gmail.com>
2023-11-19 08:03:34 -0800

    Create DateTimeExtension class

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using API.Extensions;
+
 namespace API.Entities;
 public class AppUser
 {
@@ -18,5 +20,10 @@ public class AppUser
     public string Country { get; set; }
 
     public List<Photo> Photos { get; set; } = new();
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
 

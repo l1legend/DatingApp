13eb8287e9f051c8802171946f0993e6aa8f3779
minTwin <michaelmin2020@gmail.com>
2023-12-11 07:34:37 -0800

    create member card component, modified html, css, ts file

--- client/src/app/members/members-list/members-list.component.ts
+++ client/src/app/members/members-list/members-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-members-list',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./members-list.component.css']
 })
 export class MembersListComponent {
+  members: Member[] = [];
 
+  constructor(private memberService: MembersService) {}
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: members => this.members = members
+    })
+  }
 }

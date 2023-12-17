cbd96e6a8886ed3859f0ec6e256ed0a1ddbe13ca
minTwin <michaelmin2020@gmail.com>
2023-12-17 08:36:34 -0800

    end of section 9

--- client/src/app/members/members-detail/members-detail.component.ts
+++ client/src/app/members/members-detail/members-detail.component.ts
@@ -37,7 +37,7 @@ export class MembersDetailComponent {
   getImages() {
     if (!this.member) return;
     for (const photo of this.member?.photos) {
-      this.images.push(new ImageItem({src: photo.url, thumb: photo.url}))
+      this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
     }
   }
 }
